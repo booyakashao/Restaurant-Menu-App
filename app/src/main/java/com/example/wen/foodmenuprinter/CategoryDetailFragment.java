1c40c32ab0234b14c90afcbad63a33246ef8d3d7
Wen Gu <guw@sunyit.edu>
2015-08-18 13:04:21 -0400

    Added Basic Expandable List View implementation

--- app/src/main/java/com/example/wen/foodmenuprinter/CategoryDetailFragment.java
+++ app/src/main/java/com/example/wen/foodmenuprinter/CategoryDetailFragment.java
@@ -5,13 +5,17 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.com.wen.utilities.ExpandableListAdapter;
+import com.wen.database.dao.CategoryDAO;
 import com.wen.database.dao.MenuDAO;
 import com.wen.database.model.Menu_Item;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -21,8 +25,16 @@ import java.util.List;
  * on handsets.
  */
 public class CategoryDetailFragment extends Fragment {
-    MenuDAO menuDAO;
-    List<Menu_Item> menu_items;
+    protected MenuDAO menuDAO;
+    protected CategoryDAO categoryDAO;
+    protected List<Menu_Item> menu_items;
+
+    //Expandable List Example================================================
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listMenuItemHeader;
+    private HashMap<String, List<String>> listMenuItemSubView;
+    //========================================================================
 
     /**
      * The fragment argument representing the item ID that this fragment
@@ -42,6 +54,7 @@ public class CategoryDetailFragment extends Fragment {
         super.onCreate(savedInstanceState);
         menu_items = new ArrayList<Menu_Item>();
         menuDAO = new MenuDAO(getActivity());
+        categoryDAO = new CategoryDAO(getActivity());
 
         if (getArguments().containsKey(ARG_ITEM_ID)) {
             // Load the dummy content specified by the fragment
@@ -53,20 +66,39 @@ public class CategoryDetailFragment extends Fragment {
         }
     }
 
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listMenuItemHeader = new ArrayList<String>();
+        listMenuItemSubView = new HashMap<String, List<String>>();
+
+        for(Menu_Item currentMenuItem : menu_items) {
+            listMenuItemHeader.add(currentMenuItem.getName());
+            List<String> detailList = new ArrayList<String>();
+            detailList.add(currentMenuItem.getDescription());
+            detailList.add(currentMenuItem.getPrice().toString());
+            detailList.add(categoryDAO.getCategoryById(currentMenuItem.getCategory().getId()).getName());
+            listMenuItemSubView.put(currentMenuItem.getName(),detailList);
+        }
+    }
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_category_detail, container, false);
 
         LinearLayout fragmentLinearLayout = (LinearLayout) rootView.findViewById(R.id.fragment_category_detail_linear_layout);
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
-        for(Menu_Item currentMenuItem : menu_items) {
-            TextView newTextView = new TextView(getActivity());
-            newTextView.setText(currentMenuItem.getName());
-            newTextView.setId(TextView.generateViewId());
-            newTextView.setLayoutParams(params);
-            fragmentLinearLayout.addView(newTextView);
-        }
+        // get the listview
+        expListView = (ExpandableListView) rootView.findViewById(R.id.exListView);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(getActivity(), listMenuItemHeader, listMenuItemSubView);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 
         return rootView;
     }
