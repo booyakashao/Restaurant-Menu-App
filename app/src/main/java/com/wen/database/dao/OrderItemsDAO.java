18cf5f97abda6fcfeffc9b802f7602bd8c883302
Wen Gu <guw@sunyit.edu>
2015-09-19 00:23:51 -0400

    setup

--- app/src/main/java/com/wen/database/dao/OrderItemsDAO.java
+++ app/src/main/java/com/wen/database/dao/OrderItemsDAO.java
@@ -1,6 +1,8 @@
 package com.wen.database.dao;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
 
 import com.wen.database.DatabaseUtilities;
 
@@ -14,8 +16,22 @@ public class OrderItemsDAO extends DatabaseUtilities {
     }
 
     public boolean createNewOrderItem() {
+        SQLiteDatabase db = this.getWritableDatabase();
 
+        /*
+        This is not done yet!!!
+         */
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(ORDER_ITEM_COL_2, "");
+        contentValues.put(ORDER_ITEM_COL_3, 0);
+        contentValues.put(ORDER_ITEM_COL_4, 1);
 
-        return false;
+        long result = db.insert(ORDER_ITEM_TABLE_NAME, null, contentValues);
+
+        if(result == -1) {
+            return false;
+        } else {
+            return true;
+        }
     }
 }
